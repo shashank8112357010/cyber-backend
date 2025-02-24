9b78305d0a5e2736c991d708699cf85e0be4e6a4
shashank8112357010 <shashanksharma1235999@gmail.com>
2025-02-24 16:31:24 +0530

    added

--- models/Testimonial.js
+++ models/Testimonial.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const testimonialSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
+    designation: { type: String, required: true },
     message: { type: String, required: true },
     createdAt: { type: Date, default: Date.now }
   },
